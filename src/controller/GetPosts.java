d271ef8a7c27d135bc36473a3e2c7a0cc3e6f7cc
SeanTheSheepCS <seankenny6688@gmail.com>
2020-03-10 14:59:59 -0600

    STRUCTURE & SQL FIXED???? DO NOT GITIGNORE CLASSPATH

---
+++ src/controller/GetPosts.java
@@ -0,0 +1,51 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Post;
+
+/**
+ * Servlet implementation class GetPosts
+ */
+@WebServlet("/GetPosts")
+public class GetPosts extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetPosts() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("In getposts");
+		String info = "PostID, Votenum, Title, Text/Description, title*PostID2, Votenum2, Title2, Text/Description2, title2";
+		Post posts = new Post(); 
+		info = posts.getPosts();
+		String image = posts.getImage();
+		request.setAttribute("image", image);
+		System.out.println(info);
+		request.setAttribute("allPosts", info);
+		request.getRequestDispatcher("home.jsp").forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
