6edc47656ce78f5c4162fde1d055d0f2fa573ae7
SeanTheSheepCS <seankenny6688@gmail.com>
2020-03-15 11:36:56 -0600

    Upvote and downvote buttons work!

--- src/controller/SignupUser.java
+++ src/controller/SignupUser.java
@@ -10,82 +10,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.Model;
+import model.User;
 
+/**
+ * Servlet implementation class SignupUser
+ */
 @WebServlet("/SignupUser")
 public class SignupUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	Model model;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	public SignupUser() {
-		super();
-		model = new Model();
-	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String uName = request.getParameter("uname");
 		String pwd = request.getParameter("pwd");
 		String email = request.getParameter("mail");
 		String pwdConfirm = request.getParameter("pwd-confirm");
-		System.out.println(uName + " " + email + " " + " " + pwd + " " + pwdConfirm);
-
-		boolean exist = model.checkUserExist(uName);
-		if (exist == true) {
-			request.setAttribute("userfail", uName);
-			request.getRequestDispatcher("signup.jsp").forward(request, response);
-		}
-
-		else if (pwdConfirm.contentEquals(pwd) == false) {
-			request.setAttribute("pwdfail", pwd);
-			request.getRequestDispatcher("signup.jsp").forward(request, response);
-		} else if (uName.isEmpty() || pwd.isEmpty() || email.isEmpty() || pwdConfirm.isEmpty()) {
-			request.setAttribute("emptyfail", "Empty");
-			request.getRequestDispatcher("signup.jsp").forward(request, response);
-		}
-
-		else {
-			model.addUser(uName, pwd, email);
-			HttpSession session = request.getSession();
-			session.setAttribute("username", uName);
-			request.setAttribute("succ", uName);
-			RequestDispatcher rd = request.getRequestDispatcher("signup.jsp");
-			rd.forward(request, response);
-		}
-
-	}
+		System.out.println(uName + " " +  email + " "  + " " + pwd + " " + pwdConfirm);
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String uName = request.getParameter("uname");
-		String pwd = request.getParameter("pwd");
-		String email = request.getParameter("mail");
-		String pwdConfirm = request.getParameter("pwd-confirm");
-		System.out.println(uName + " " + email + " " + " " + pwd + " " + pwdConfirm);
-
-		boolean exist = model.checkUserExist(uName);
+		User user = new User();
+		boolean exist = user.checkUserExist(uName);
 		if (exist == true) {
 			request.setAttribute("userfail", uName);
 			request.getRequestDispatcher("signup.jsp").forward(request, response);
-		}
-
-		else if (pwdConfirm.contentEquals(pwd) == false) {
+			}
+		
+		else if(pwdConfirm.contentEquals(pwd) == false) {
 			request.setAttribute("pwdfail", pwd);
 			request.getRequestDispatcher("signup.jsp").forward(request, response);
-		} else if (uName.isEmpty() || pwd.isEmpty() || email.isEmpty() || pwdConfirm.isEmpty()) {
+		}
+		else if(uName.isEmpty() || pwd.isEmpty() || email.isEmpty() || pwdConfirm.isEmpty()) {
 			request.setAttribute("emptyfail", "Empty");
 			request.getRequestDispatcher("signup.jsp").forward(request, response);
 		}
 
 		else {
-			model.addUser(uName, pwd, email);
+			user.addUser(uName, pwd, email);
 			HttpSession session = request.getSession();
 			session.setAttribute("username", uName);
 			request.setAttribute("succ", uName);
 			RequestDispatcher rd = request.getRequestDispatcher("signup.jsp");
 			rd.forward(request, response);
 		}
-
+		
 	}
 
 }
--- src/controller/ValidateUser.java
+++ src/controller/ValidateUser.java
@@ -1,6 +1,14 @@
 package controller;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,51 +17,74 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.Model;
+import model.User;
 
+/**
+ * Servlet implementation class ValidateUser
+ */
 @WebServlet("/ValidateUser")
 public class ValidateUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	Model model;
 
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
 	public ValidateUser() {
 		super();
-		model = new Model();
 	}
-	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String uName = request.getParameter("uname");
-		String uPass = request.getParameter("upass");
-
-		if (model.validateUser(uName, uPass)) {
-			HttpSession session = request.getSession();
-			session.setAttribute("username", uName);
-			response.sendRedirect(request.getContextPath() + "/GetTopics");
-
-		}
-
-		else {
-			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
-			rd.forward(request, response);
-		}
+		doPost(request, response);
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String uName = request.getParameter("uname");
 		String uPass = request.getParameter("upass");
-
-		if (model.validateUser(uName, uPass)) {
+		User user = new User();
+		if (user.validateUser(uName, uPass)) {
 			HttpSession session = request.getSession();
 			session.setAttribute("username", uName);
-			response.sendRedirect(request.getContextPath() + "/GetTopics");
+			response.sendRedirect(request.getContextPath() + "/GetPosts");
+			ScriptEngineManager manager = new ScriptEngineManager();
+			ScriptEngine engine = manager.getEngineByName("JavaScript");
+			// read script file
+			try {
+				engine.eval(Files.newBufferedReader(Paths.get("C:/Scripts/Jsfunctions.js"), StandardCharsets.UTF_8));
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-		}
+			Invocable inv = (Invocable) engine;
+			// call function from script file
+			try {
+				inv.invokeFunction("yourFunction", "param");
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			}
 
 		else {
 			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
 			rd.forward(request, response);
 		}
 	}
+
 }
