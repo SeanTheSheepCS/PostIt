163fb13c5a92eda3ae452bda87ff7fabe2e736cd
SeanTheSheepCS <seankenny6688@gmail.com>
2020-03-21 16:18:02 -0600

    Fixed compiler error

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         //ServletOutputStream out = resp.getOutputStream();
         request.getRequestDispatcher("src/main/webapp/hello.jsp").forward(request, response);
