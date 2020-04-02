b1cd8e75a7eae35d566c56197634e1b9ea453f81
ummeyt <35542644+ummeyt@users.noreply.github.com>
2020-04-01 22:40:05 -0600

    Add files via upload

---
+++ src/main/webapp/js/favButton.js
@@ -0,0 +1,39 @@
+function favPostOne(x) {
+    x.classList.toggle(document.getElementById("heart-one").style.color = "red");
+}
+
+function favPostTwo(x) {
+    x.classList.toggle(document.getElementById("heart-two").style.color = "red");
+}
+
+function favPostThree(x) {
+    x.classList.toggle(document.getElementById("heart-three").style.color = "red");
+}
+
+function favPostFour(x) {
+    x.classList.toggle(document.getElementById("heart-four").style.color = "red");
+}
+
+function favPostFive(x) {
+    x.classList.toggle(document.getElementById("heart-five").style.color = "red");
+}
+
+function favPostSix(x) {
+    x.classList.toggle(document.getElementById("heart-six").style.color = "red");
+}
+
+function favPostSeven(x) {
+    x.classList.toggle(document.getElementById("heart-seven").style.color = "red");
+} 
+
+function favPostEight(x) {
+    x.classList.toggle(document.getElementById("heart-eight").style.color = "red");
+}
+
+function favPostNine(x) {
+    x.classList.toggle(document.getElementById("heart-nine").style.color = "red");
+}
+
+function favPostTen(x) {
+    x.classList.toggle(document.getElementById("heart-ten").style.color = "red");
+}
